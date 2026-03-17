aee77638e7ed2a1c62856e53fdb4943d3f4f709c
OP-Devloop <oscar.pettersson@iths.se>
2026-03-17 09:00:13 +0100

    Implement WebSocket server and update servlet and JSP for chat functionality

--- src/main/java/se/iths/oscarp/jakartaassignmentwebsocketserver/WebSocketServer.java
+++ src/main/java/se/iths/oscarp/jakartaassignmentwebsocketserver/WebSocketServer.java
@@ -1,4 +1,38 @@
 package se.iths.oscarp.jakartaassignmentwebsocketserver;
 
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+@ServerEndpoint("/chat")
 public class WebSocketServer {
+
+    private static Set<Session> sessions = new CopyOnWriteArraySet<>();
+
+    @OnOpen
+    public void onOpen(Session session) {
+        sessions.add(session);
+        System.out.println("Client connected");
+    }
+
+    @OnMessage
+    public void onMessage(String message, Session session) {
+
+        for (Session s : sessions) {
+            if (s.isOpen()) {
+                s.getAsyncRemote().sendText(message);
+            }
+        }
+    }
+
+    @OnClose
+    public void onClose(Session session) {
+        sessions.remove(session);
+        System.out.println("Client disconnected");
+    }
 }
